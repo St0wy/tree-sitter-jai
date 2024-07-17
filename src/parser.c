f309990aec0e0d7b07f71108702117aa17fa96b3
Stowy <stowy@protonmail.ch>
2024-07-17 18:59:01 +0200

    Removed capital from Jai

--- src/parser.c
+++ src/parser.c
@@ -962,7 +962,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_Jai(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_jai(void) {
   static const TSLanguage language = {
     .version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
